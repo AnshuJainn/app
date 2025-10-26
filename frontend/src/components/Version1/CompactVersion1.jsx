185da7f61d71672e0fee256e226ddba146ec31aa
Anshu <jain30anshu1997@gmail.com>
2025-10-26 23:54:30 +0530

    Update CompactVersion1.jsx

--- frontend/src/components/Version1/CompactVersion1.jsx
+++ frontend/src/components/Version1/CompactVersion1.jsx
@@ -4,7 +4,7 @@ import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Textarea } from '../ui/textarea';
 import { useToast } from '../../hooks/use-toast';
-import { PortfolioData } from '../../mockData';
+import { portfolioData } from '../../mockData';
 import { Toaster } from '../ui/toaster';
 
 const CompactVersion1 = () => {
